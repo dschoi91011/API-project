7079042d7c9aed6e5d2d8ad3471ffda29f67590e
Daniel <daniel.choi91011@gmail.com>
2024-03-28 13:08:36 -0700

    Add User authentication Express middleware

--- backend/db/seeders/20240328174727-demo-user.js
+++ backend/db/seeders/20240328174727-demo-user.js
@@ -22,18 +22,18 @@ module.exports = {
     await User.bulkCreate([
       {
         email: 'demo@user.io',
-        username: 'Demo-Dan',
+        username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
         email: 'user1@user.io',
-        username: 'Fake1',
-        hashedPassword: bcrypt.hashSync('password1')
+        username: 'FakeUser1',
+        hashedPassword: bcrypt.hashSync('password2')
       },
       {
         email: 'user2@user.io',
-        username: 'Fake2',
-        hashedPassword: bcrypt.hashSync('password2')
+        username: 'FakeUser2',
+        hashedPassword: bcrypt.hashSync('password3')
       }
     ], {validate: true});
 
@@ -49,7 +49,7 @@ module.exports = {
     options.tableName = 'Users';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      username: {[Op.in]: ['Demo-Dan', 'Fake1', 'Fake2']}
+      username: {[Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2']}
     }, {});
   }
 };
