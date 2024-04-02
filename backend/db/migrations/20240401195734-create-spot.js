61f930095f2d6c8be71b565cbddf363a883f3bba
Daniel <daniel.choi91011@gmail.com>
2024-04-01 17:10:32 -0700

    added validations to Spot

--- backend/db/migrations/20240401195734-create-spot.js
+++ backend/db/migrations/20240401195734-create-spot.js
@@ -14,16 +14,20 @@ module.exports = {
         references: {model: 'Users'}
       },
       address: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       city: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       state: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       country: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       lat: {
         type: Sequelize.DECIMAL
@@ -32,10 +36,11 @@ module.exports = {
         type: Sequelize.DECIMAL
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(50)
       },
       description: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       price: {
         type: Sequelize.DECIMAL
