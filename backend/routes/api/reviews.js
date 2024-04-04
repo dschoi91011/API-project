5231714b47119ba9d4085b584922560cd5e3a216
Daniel <daniel.choi91011@gmail.com>
2024-04-03 17:11:17 -0700

    added route

---
+++ backend/routes/api/reviews.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+
+const {setTokenCookie, requireAuth} = require('../../utils/auth');
+const {User, Spot, Booking, Review, ReviewImage, SpotImage} = require('../../db/models');
+
+const {check} = require('express-validator');
+const {handleValidationErrors} = require('../../utils/validation');
+
+const router = express.Router();
+
+//Get all reviews of current user-------------------------------------------
+//Extract previewImage
+router.get('/current', requireAuth, async (req, res, next) => {
+    const {user} = req;
+    const allReviews = await Review.findAll({
+        where: {userId: user.id},
+        include: [
+            {
+                model: User,
+                attributes: ['id', 'firstName', 'lastName']
+            },
+            {
+                model: Spot,
+                attributes: [
+                    'id',
+                    'ownerId',
+                    'address',
+                    'city',
+                    'state',
+                    'country',
+                    'lat',
+                    'lng',
+                    'name',
+                    'price',
+                    //previewImage
+                ]
+            },
+            {
+                model: ReviewImage,
+                attributes: ['id', 'url']
+            }
+        ]
+    });
+
+    res.json({Reviews: allReviews});
+});
+
+
+
+module.exports = router;
