f138f6e84f3ad8279bc9a3a5f0c9001327dbd36e
Daniel <daniel.choi91011@gmail.com>
2024-04-25 02:32:21 -0700

    working on delete modal

--- frontend/src/components/DeleteFormModal/DeleteFormModal.jsx
+++ frontend/src/components/DeleteFormModal/DeleteFormModal.jsx
@@ -1,15 +1,21 @@
-
+import { useModal } from '../../context/Modal';
+import { useDispatch } from 'react-redux';
 
 function DeleteFormModal(){
+const {closeModal} = useModal()
+const dispatch = useDispatch()
 
+const handleSubmit = (e) => {
+    e.preventDefault()
+}
 
     return(
-        <div>
+        <form onSubmit={handleSubmit}>
             <h1>Confirm Delete</h1>
             <p>Are you sure you want to remove this spot?</p>
             <button>Yes (Delete Spot)</button>
             <button>No (Keep Spot)</button>
-        </div>
+        </form>
 
     )
 
